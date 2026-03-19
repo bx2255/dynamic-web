9bf12890adefc86f884916baa357522cc5d68b61
bx2255 <bx2255@nyu.edu>
2026-03-19 02:05:16 -0400

    Refactor project initialization and API responses

--- 4/server.js
+++ 4/server.js
@@ -2,35 +2,35 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-app.use(express.static("public"));// let server read json and public files
-
+app.use(express.static("public"));
 let projects = [
     {
         id: Date.now(),
-        title: "Test Project",
+        title: "Initial Project",
         year: 2026,
-        description:"This is a test project description.",     
+        description: "Memory storage test",     
     }
-]//this is my Json data for testing(in memory, not in a file)
+];
 
+// GET
 app.get("/api/projects", (req, res) => {
     res.json(projects);
-});//read all projects
+});
 
-
+// POST
 app.post("/api/projects", (req, res) => {
     const newProject = {
         id: Date.now(),
         title: req.body.title || "Untitled",
-        year: Number(req.body.year)||2026,
-        description: req.body.description||"",
+        year: Number(req.body.year) || 2026,
+        description: req.body.description || "",
     };
 
     projects.unshift(newProject);
-    res.json({ok: true, project: newProject});
+    res.json({ ok: true, project: newProject });
 });
 
-// open server on port 8000
+// listen 8000 port
 app.listen(8000, () => {
   console.log("Server running at http://localhost:8000");
 });
